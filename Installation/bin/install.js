0e1b2e40b3b954b50d15401403808b0e8f273c4a
netdb <byh741@naver.com>
2020-01-10 20:07:33 +0900

    update!

--- Installation/bin/install.js
+++ Installation/bin/install.js
@@ -3,32 +3,9 @@
 const program = require('commander');
 const javaAction = require('../lib/java.js')
 const sshpassAction = require('../lib/sshpass.js')
-//
-// console.log('3');
 
-// program
-//     .command('package')
-//     .arguments('<java|sshpass>')
-//     .action(function installPackage(opts){
-//       // console.log('4');
-//       switch(opts){
-//         case 'java' :
-//           javaAction.javaInstall();
-//           break;
-//
-//         case 'sshpass' :
-//           sshpassAction.sshpassInstall();
-//           break;
-//       }
-//     })
-//
-// program.parse(process.argv);
-
-
-
-module.exports.installPackage = (opts) => {
-  console.log('ww');
-  switch(opts){
+module.exports.installPackage = (opt) => {
+  switch(opt){
          case 'java' :
            javaAction.javaInstall();
            break;
--- index.js
+++ index.js
@@ -2,62 +2,15 @@
 
 const program = require('commander');
 const installation = require('./Installation/bin/install.js')
-// const javaAction = require('./Installation/lib/java.js')
-// const sshpassAction = require('./Installation/lib/sshpass.js')
 // const bencmhark = require('./Benchmark/bin/BMrun.js')
-// console.log('1');
-
-// install.installpackage();
-
-
-// program
-//     .command('install <package|database> <java|sshpass>')
-//     // .command('install <database> <dbname>')
-//     // .command('benchmark')
-//     .action((opt1, opt2) => {
-//
-//       if(opt1 == 'package'){
-//         console.log('package!!');
-//       }else{
-//         console.log('database!!');
-//       }
-//
-//       if(opt2 == 'java'){
-//         console.log('java@');
-//       }else{
-//         console.log('sshpass');
-//       }
-//         // java, sshpass
-//     })
-
-    //   function install(){
-    //   const install = require('./Installation/bin/install.js')
-    //   install
-    //   console.log('2');
-    // })
-
-
-
-// console.log(property.getNodeInfo());
-
-// program
-//   .command('install <package> <java|sshpass>')
-//   .action(function install(opt1, opt2){
-//     if(opt2 == 'java'){
-//       // console.log('java');
-//       javaAction.javaInstall()
-//
-//     }else{
-//       // console.log('ssh');
-//       sshpassAction.sshpassInstall();
-//     }
-//   })
 
 
 program
   .command('install')
-  .option('-p, --package <java|sshpass>')
+  .arguments('<opt>')
+  .option('-p, --package')
   .action(function installPackage(opt){
+    //console.log(opt);
     installation.installPackage(opt);
   })
 
@@ -67,9 +20,6 @@ program.parse(process.argv);
 
 
 
-//
-// program
-//   .command('install')
 
 // program
 //     .command('benchmark')
