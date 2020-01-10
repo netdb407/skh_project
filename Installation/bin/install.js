b44178babb1ff20f61e910180bdbbf67d11814c5
netdb <byh741@naver.com>
2020-01-10 17:44:04 +0900

    Initial commit

---
+++ Installation/bin/install.js
@@ -0,0 +1,34 @@
+
+
+const program = require('commander');
+const javaAction = require('./Installation/lib/java.js')
+const sshpassAction = require('./Installation/lib/sshpass.js')
+
+console.log('3');
+
+program
+    .command('package')
+    .arguments('<java|sshpass>')
+    .action(function installPackage(opts){
+      console.log('4');
+      switch(opts){
+        case 'java' :
+          javaAction.javaInstall();
+          break;
+
+        case 'sshpass' :
+          sshpassAction.sshpassInstall();
+          break;
+      }
+    })
+
+
+
+
+program.parse(process.argv);
+
+
+
+module.exports.installPackage = () => {
+  console.log('ww');
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+const javaAction = require('./Installation/lib/java.js')
+const sshpassAction = require('./Installation/lib/sshpass.js')
+// const bencmhark = require('./Benchmark/bin/BMrun.js')
+// console.log('1');
+
+// install.installpackage();
+
+
+// program
+//     .command('install <package|database> <java|sshpass>')
+//     // .command('install <database> <dbname>')
+//     // .command('benchmark')
+//     .action((opt1, opt2) => {
+//
+//       if(opt1 == 'package'){
+//         console.log('package!!');
+//       }else{
+//         console.log('database!!');
+//       }
+//
+//       if(opt2 == 'java'){
+//         console.log('java@');
+//       }else{
+//         console.log('sshpass');
+//       }
+//         // java, sshpass
+//     })
+
+    //   function install(){
+    //   const install = require('./Installation/bin/install.js')
+    //   install
+    //   console.log('2');
+    // })
+
+
+program
+  .command('install <package> <java|sshpass>')
+  .action(function install(opt1, opt2){
+    if(opt2 == 'java'){
+      console.log('java');
+      javaAction.javaInstall()
+
+    }else{
+      console.log('ssh');
+      sshpassAction.sshpassInstall();
+    }
+  })
+
+
+
+
+
+program.parse(process.argv);
+
+
+
+
+//
+// program
+//   .command('install')
+
+// program
+//     .command('benchmark')
+//     .action(function benchmark(){
+          //const bencmhark = require('./Benchmark/bin/BMrun.js')
+//       bencmhark
+//     })
+//
+// program.parse(process.argv);
