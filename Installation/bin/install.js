092fb476dbec1b73437e831814b200b782db0146
netdb <byh741@naver.com>
2020-01-14 20:19:32 +0900

    promise적용중..

--- Installation/bin/install.js
+++ Installation/bin/install.js
@@ -2,10 +2,12 @@ const program = require('commander');
 const execFile = require('child_process').execFile;
 const exec = require('child_process').execSync;
 const property = require('../../propertiesReader.js')
+const versionChecking = require('./versionCheck.js')
 const javaAction = require('../lib/java.js')
 const sshpassAction = require('../lib/sshpass.js')
-const versionChecking = require('./versionCheck.js')
-
+const serverInfo = property.get_server_install_dir()
+const nodeInfo = property.get_node_install_dir()
+var pckInfo = null
 
 program
   .command('install')
@@ -14,20 +16,26 @@ program
   .option('-s, --server', `server에 설치, -p 옵션에만 적용`)
   .option('-n, --node', `node에 설치, -p 옵션에만 적용`)
   .action(function Action(opt){
+    pckInfo = opt.package
 
-    versionChecking.versionCheck(opt.package);
-    console.log('server :', opt.server);
-    console.log('node : ', opt.node);
+    // var start = 1;
+    versionChecking.versionCheck(pckInfo)
+    //   .then(result => {
+    //     console.log(result);
+    //   });
 
-    console.log('있으면 true, 없으면 false : ', versionChecking.haveArg);
+    // console.log('server :', opt.server);
+    // console.log('node : ', opt.node);
+
+    // console.log('있으면 true, 없으면 false : ', versionChecking.haveArg);
+
+
     //dir는 들어오면 true, 안들어오면 false
     //dir중 not null인 것 을 인자로 넣어야 함..
 
     if(typeof opt.server != "undefined"){
-      const serverInfo = property.get_server_install_dir()
       installPackage(opt.package, serverInfo)
     }else if(typeof opt.node != "undefined"){
-      const nodeInfo = property.get_node_install_dir()
       installPackage(opt.package, nodeInfo)
     }
   })
@@ -36,11 +44,17 @@ program.parse(process.argv)
 
 
 
-function installPackage(package, dir){
-  console.log('dir정보 뜨기');
-  console.log(dir);
+// function test(){
+//   Promise.all([versionChecking.versionCheck(pckInfo), versionChecking.changeHaveArg()]).then(
+//     function(values){
+//       console.log(values);
+//     }
+//   )
+// }
 
-  switch(package){
+function installPackage(package, dir){
+  console.log('dir정보 : ', dir);
+  switch(package, dir){
       case 'java' :
         javaAction.javaInstall(package);
         break;
--- Installation/bin/versionCheck.js
+++ Installation/bin/versionCheck.js
@@ -4,12 +4,20 @@ const haveArg = false;  //false: 설치안됨, true: 설치됨
 
 
 
-function versionCheck(arg){
+ function versionCheck(arg){
   const child = execFile(arg, (err, stdout, stderr) => {
     if(Object.keys(err).includes('errno')==true){
       console.log('존재 유무 : 설치되지 않았습니다.');
+      console.log('설치 시작');
+      // return new Promise(function(resolve, reject){
+      //   setTimeout(function(){
+      //     resolve(false)
+      //   }, 1000)
+      // })
+      haveArgIsFalse();
     }
     else if(typeof stderr == 'string'){
+
       //설치안됨 : stderr에 아무것도 안담김
       //설치됨 : typeof stderr == string
 
@@ -17,14 +25,48 @@ function versionCheck(arg){
       // console.log(stdout);
       // console.log('stdout');
       // console.log(stderr);
-      console.log('haveArg 좀 바뀌어라');
-      const haveArg = true;
+      // console.log('haveArg 좀 바뀌어라');
+      // const haveArg = true;
+
       console.log('존재 유무 : 이미 존재하는 패키지입니다.');
+      console.log('실행 종료');
+
+      haveArgIsTrue();
     }
   })
 }
 
+// function changeHaveArg(){
+//   // const haveArg = true;
+//   return new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//       resolve(true)
+//     }, 1000)
+//   })
+// }
+
+function haveArgIsTrue(){
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(true)
+    }, 1000)
+  })
+}
+
+
+function haveArgIsFalse(){
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(false)
+    }, 1000)
+  })
+}
+
+
 module.exports = {
   haveArg,
-  versionCheck
+  versionCheck,
+
+  haveArgIsTrue,
+  haveArgIsFalse
 }
