aa9e871c66203986f760f45283d42435ed52d861
netdb <byh741@naver.com>
2020-03-24 21:22:07 +0900

    Update cmds.js

--- Installation/lib/cmds.js
+++ Installation/lib/cmds.js
@@ -7,34 +7,6 @@
 // maven install : rpm -ivh 파일명(dependencies 쩔음)
 // maven delete : rpm -e maven
 
-//[설치 테스트 순서] - git, java, mvn, python
-//1. java, git은 지워보고
-//2. mvn은 폴더 자체를 삭제?? /etc/profile 수정은?
-//3. python은 심볼릭 링크 삭제?
-
-//[java]
-//<<java 지울 때 한번에 안 지워지는 문제>> : 한개씩 지워야 함..
-//***conflicting request : perl 모듈 필요하다는 오류 나면 : yum upgrade 해보기
-// yum --version 하면 4.2.7 나오는데 이게 최신인가?<해결불가..>
-//yum remove -y java* 로 해서 dependency 오류 나면
-//rpm -qa|grep java 해서 나오는 결과 한개씩 yum으로 지워줘야할 것 같은데..
-
-//tzdata-java 강제로 한개 지운 경우(194, 195) : 설치 시에도 dependency 에러 발생;;;
-//강제로 다시 설치해줘야할 것 같은데..
-
-
-//[python]
-//심볼링링크 /usr/bin에서 찾고 지우고 ..<명령어 찾기!>\
-
-
-
-//[테스트 결과]
-//1. git (o)
-//2. java (x) : 강제로 한개씩 해버리면 ㄱㅊ
-//3. maven (x!!) : 뭔 문제냐~~ /etc/profile 문제일듯..
-//4. python
-
-
 const installCmd = 'rpm -ivh'
 const deleteCmd = 'rpm -e' //git, sshpass
 const yumDeleteCmd = 'yum remove -y'
