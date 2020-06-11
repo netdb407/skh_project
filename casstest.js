1605aa9052f162945388bf0b4b2e186707a9c39f
netdb <byh741@naver.com>
2020-06-11 17:34:03 +0900

    match로 수정

--- casstest.js
+++ casstest.js
@@ -2,13 +2,16 @@
 
 const property = require('./propertiesReader.js')
 const exec =  require('child_process').exec
+const childProcess = require("child_process");
 const chalk = require('chalk')
 let nodeIPArr //split array
 let node_ip = property.get_nodes_IP();
 let nodetool_ip = property.get_nodetool_IP();
 nodeIPArr = node_ip.split(',');
 let tempArray = []
-let found = []
+let statusArr = []
+var Promise = require('promise');
+
 
 main()
 
@@ -16,51 +19,204 @@ function main(){
   for(var ip of nodeIPArr){
     console.log('----------------------------------------------------------');
     console.log(chalk.green.bold('[INFO]'), 'IP address', chalk.blue.bold(ip));
-    stopfirewalld(ip);
-    runCassandra(ip);
+   runCassandra(ip);
+   // test(ip);
   }
   console.log('----------------------------------------------------------');
   console.log(chalk.green.bold('[INFO]'), 'IP address', chalk.blue.bold(nodetool_ip));
   console.log(chalk.green.bold('[INFO]'), 'check Node Status');
+  var testarr = makeStatusArr(nodetool_ip)
+  testarr.then((res) => {
+    console.log('in main', res);
+    checkNodeStatus(nodetool_ip)
+  })
+  .catch((err)=>{
+    console.log('in main', err);
+  })
 
-  checkNodeStatus(nodetool_ip);
- // while(found.length<3){
 
-   if(found.length<3){
-      console.log(chalk.yellow.bold('[INFO]'), 'Waiting for cassandra ON ...');
-  //   setTimeout(checkNodeStatus(nodetool_ip), 3000);
-  }else{
-    console.log(chalk.blue.bold('[INFO]'), 'Now you can use cassandra from cluster nodes')
-  }
+
+
+
+    // checkNodeStatus(nodetool_ip).then((res) => {
+    //   console.log('res : \n', res);
+    //   return checkNodeStatus(nodetool_ip);
+    // }).then((res)=>{
+    //   console.log('promise어렵다');
+    //   return console.log('다른함수쓰기');
+    // })
+    // console.log('statusArr in main', statusArr);
+    // .then(function(result) {
+    //   console.log('result in main', result);
+    //   return result + 10;
+    // })
+    // .catch(function(err) {
+    //   console.log(err);
+    // })
+
+  // console.log('in main', statusArr);
+  // console.log('statusArrlength : ', statusArr.length);
+//  while(statusArr.length<2){
+//    checkNodeStatus(nodetool_ip)
+//  }
+  //console.log(chalk.blue.bold('[INFO]'), 'Now you can use cassandra from cluster nodes')
+  // try{
+  //  execute(checkNodeStatus(nodetool_ip));
+  // }catch(e){
+  //  //console.log(e)
+  // };
+ // while(statusArr.length<3){
+ //  console.log(chalk.yellow.bold('[INFO]'), 'Waiting for cassandra ON ...');
+  // if(statusArr.length<3){
+//   setTimeout(checkNodeStatus(nodetool_ip), 3000);
   // }
+  // }
+
+
+
+
 }
 
-function stopfirewalld(ip){
-  try{
-    exec(`ssh root@${ip} systemctl stop firewalld`)
-    console.log(chalk.green.bold('[INFO]'), 'stop firewall');
-  }
-  catch(e){ console.log(e); }
+
+
+
+
+
+
+
+function on_child_stdout(data) {
+ 	var str = data.toString(),
+        lines = str.split(/\n/g);
+	for (var i in lines) {
+	  if (! lines[i]) {
+        console.log(lines[i]);
+	  }
+	}
+};
+
+function execute(command) {
+  /**
+   * @param {Function} resolve A function that resolves the promise
+   * @param {Function} reject A function that fails the promise
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+   */
+  return new Promise(function(resolve, reject) {
+    /**
+     * @param {Error} error An error triggered during the execution of the childProcess.exec command
+     * @param {string|Buffer} standardOutput The result of the shell command execution
+     * @param {string|Buffer} standardError The error resulting of the shell command execution
+     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+     */
+    childProcess.exec(command, function(error, standardOutput, standardError) {
+      if (error) {
+        reject();
+        return;
+      }
+      if (standardError) {
+        reject(standardError);
+        return;
+      }
+      resolve(standardOutput);
+    });
+  });
 }
+
+
 
 function runCassandra(ip){
-  try{
+  firewallcmd = `ssh root@${ip} systemctl stop firewalld`
+  killcmd = `ssh root@${ip} /root/ssdStorage/cassandra/killCass.sh`
+  runcmd = `ssh root@${ip} /root/ssdStorage/cassandra/bin/cassandra -fR`
+  // console.log('firewallcmd : ', firewallcmd);
+  // try{
+  //  await execute(firewallcmd);
+   console.log(chalk.green.bold('[INFO]'), 'stop firewall in', `${ip}`);
+  // }catch(e){
+  //  //console.log(e)
+  // };
+  // try{
+  //  await execute(killcmd);
+  // }catch(e){
+  //  //console.log(e)
+  // };
+  // try{
+  //  await execute(runcmd);
+  // }catch(e){
+  // //console.log(e)
+  // };
+  // exec(firewallcmd)
+  // //exec(killcmd)
+  // exec(runcmd)
 
-    exec(`ssh root@${ip} /root/ssdStorage/cassandra/bin/cassandra -fR`)
-    console.log(chalk.green.bold('[INFO]'), 'run Cassandra');
-  }
-  catch(e){ console.log(e); }
+
+  console.log(chalk.green.bold('[INFO]'), 'run Cassandra in', `${ip}`);
+// try{
+//    exec(`ssh root@${ip} /root/ssdStorage/cassandra/killCass.sh`)
+//    exec(`ssh root@${ip} /root/ssdStorage/cassandra/bin/cassandra -fR`)
+//    console.log(chalk.green.bold('[INFO]'), 'run Cassandra');
+//  }
+//  catch(e){ console.log(e); }
 }
 
-function checkNodeStatus(nodetoolIP){
+
+// let myFirstPromise = new Promise((resolve, reject) => {
+//   // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+//   // In this example, we use setTimeout(...) to simulate async code.
+//   // In reality, you will probably be using something like XHR or an HTML5 API.
+//   setTimeout(function(){
+//     resolve("Success!"); // Yay! Everything went well!
+//   }, 250);
+// });
+//
+// myFirstPromise.then((successMessage) => {
+//   // successMessage is whatever we passed in the resolve(...) function above.
+//   // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+//   console.log("Yay! " + successMessage);
+// });
+
+
+function makeStatusArr(nodetoolIP){
   let cmd = `ssh root@${nodetoolIP} /root/ssdStorage/cassandra/bin/nodetool status`
   let checkcmd = exec(cmd)
   checkcmd.stdout.on('data', function(data){
-    //console.log('data :', data);
+    console.log('###stdout data : \n', data);
     const testdata = data.split(' ');
     tempArray.push(testdata.indexOf('UN'));
-    found = tempArray.filter(un => un == 0);
-    //console.log(found);
-    return found;
+    statusArr = tempArray.filter(un => un == 0);
+    console.log(statusArr, 'from stdout');
+    // return statusArr;
   })
+  checkcmd.stdin.on('data', function(data){
+    console.log('###stdin data : \n', data);
+    const testdata = data.split(' ');
+    tempArray.push(testdata.indexOf('UN'));
+    statusArr = tempArray.filter(un => un == 0);
+    console.log(statusArr, 'from stdin');
+    // return statusArr;
+  })
+  checkcmd.stderr.on('data', function(data){
+    console.log('###stderr data : \n', data);
+    const testdata = data.split(' ');
+    tempArray.push(testdata.indexOf('UN'));
+    statusArr = tempArray.filter(un => un == 0);
+    console.log(statusArr, 'from stderr');
+  })
+  return statusArr
+}
+
+function checkNodeStatus(nodetoolIP, callback){
+  let promiseTest = new Promise( (resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('in resolve', statusArr);
+  },3000);
+  })
+
+  console.log('promise in checkStatus', statusArr);
+
+  promiseTest.then((res) => {
+      console.log('in then..', res);
+  })
+  .catch((err) => {
+      console.log('in catch', err);
+  });
 }
---
+++ promiseTest.js
@@ -0,0 +1,148 @@
+#!/usr/bin/env node
+
+// 1. 함수 정의 : return promise 객체(UN이 3개일때 true담고, 아닐때 false를 담기)
+// 2. 변수 생성 : 1번의 함수로 생성
+// 3. 2번의 변수로 .then(성공 시 벤치마크 수행), .catch(실패 시 상태확인 재실행)
+//
+//
+//
+//
+// 소민이가 실행하는 상태확인 함수 1개 => checkCassON()
+//
+// checkCassON()함수 => return promise 객체
+// 1. 노드3개를 돌며 방화벽 해제
+// 2. 노드 3개를 돌며 카산드라 켜기(그 전에 kill?)
+// 3. 상태 출력해서 UN을 담는 배열 생성하고 배열 길이로 true/false 체크
+//
+// checkCassON()을 이용해서 변수 생성 => statusCass
+//
+// statusCass로 .then(카산드라 벤치마크 수행), .catch(checkCassON()함수 재실행)
+//
+//
+//
+// function checkCassON(nodeIP, nodetool_ip){
+//   //for문 ?
+//   1. 193, 194, 195 방화벽 해제
+//   2. 193, 194, 195 kill
+//   3. 193, 194, 195 카산드라 켜기
+//   4. nodetool 193에서 nodetool status로 상태 출력하고 배열 생성
+//   5. 배열 길이로 statusCass 변수 생성 : true/false
+// }
+//
+// statusCass.then(카산드라 벤치마킹), .catch(checkCassON()재실행)
+//
+// -----------------------------------------------------------------
+const property = require('./propertiesReader.js')
+const exec =  require('child_process').exec
+const childProcess = require("child_process");
+const chalk = require('chalk')
+let nodeIPArr //split array
+let node_ip = property.get_nodes_IP();
+let nodetool_ip = property.get_nodetool_IP();
+nodeIPArr = node_ip.split(',');
+let tempArray = []
+let statusArr = []
+var Promise = require('promise');
+let statusCass = false
+
+
+function checkCassON(nodeIPArr, nodetool_ip){
+  return new Promise(function(resolve, reject){
+    //1~3
+    nodeIPArr.forEach(function(ip){
+      let firewallcmd = `ssh root@${ip} systemctl stop firewalld`
+      // let killcmd = `ssh root@${ip} /root/ssdStorage/cassandra/killCass.sh`
+      let runcmd = `ssh root@${ip} /root/ssdStorage/cassandra/bin/cassandra -R`
+      console.log('----------------------------------------------------------');
+      console.log(chalk.green.bold('[INFO]'), 'IP address', chalk.blue.bold(ip));
+      exec(firewallcmd)
+      console.log(chalk.green.bold('[INFO]'), 'stop firewall in', `${ip}`);
+      // exec(killcmd)
+      exec(runcmd)
+      console.log(chalk.green.bold('[INFO]'), 'run Cassandra in', `${ip}`);
+    })
+    //4
+    console.log('----------------------------------------------------------');
+    console.log(chalk.green.bold('[INFO]'), 'IP address', chalk.blue.bold(nodetool_ip));
+    console.log(chalk.green.bold('[INFO]'), 'check Node Status');
+    let statuscmd = `ssh root@${nodetool_ip} /root/ssdStorage/cassandra/bin/nodetool status`
+    let checkcmd = exec(statuscmd)
+
+    let resulta = '';
+
+    checkcmd.stdout.on('data', function(data){
+      // console.log('stdout data======>', data);
+
+      resulta += data.toString();
+      // console.log('RESULTA', resulta)
+
+      let testc = resulta.match('UN')
+      if(testc !== null){
+        // let testb = resulta.match('UN');
+        tempArray.push(testc)
+      }
+      console.log('tempArray length ===>', tempArray.length);
+      // tempArray.push(testb)
+      // console.log(tempArray.length);
+
+      // const testdata = data.split(' ');
+      // tempArray.push(testdata.indexOf('UN'));
+      // statusArr = tempArray.filter(un => un == 0);
+      // console.log('===> stdout_statusArr : ', statusArr);
+      if(tempArray.length ==3){
+        statusCass = true
+      }
+      console.log('statusCass ===> : ', statusCass);
+      return resolve(statusCass);
+
+    })
+
+    checkcmd.stdin.on('data', function(data){
+      console.log('stdin data====>', data);
+      const testdata = data.split(' ');
+      tempArray.push(testdata.indexOf('UN'));
+      statusArr = tempArray.filter(un => un == 0);
+      console.log('===> stdin_statusArr : ', statusArr);
+      if(statusArr.length ==3){
+        statusCass = true
+      }
+    })
+
+    checkcmd.stderr.on('data', function(data){
+      console.log('stderr data====>', data);
+      const testdata = data.split(' ');
+      tempArray.push(testdata.indexOf('UN'));
+      statusArr = tempArray.filter(un => un == 0);
+      console.log('===> stderr_statusArr : ', statusArr);
+      if(statusArr.length ==3){
+        statusCass = true
+      }
+      return reject( console.log('error!') );
+    })
+
+  })
+}
+
+let checktest = checkCassON(nodeIPArr, nodetool_ip)
+
+
+
+checktest.then(result => {
+  console.log(result);
+  // console.log('=====> result(true/false) : \n', result);
+  // console.log('====> cassandra status : ', statusCass);
+  // console.log('wow its true! Start Cassandra Benchmarking');
+
+    if(result == true){
+      console.log('wow its true! Start Cassandra Benchmarking');
+    }else{
+      console.log('in if) Checking Cassandra status again..');
+    }
+  })
+  .catch(err => {
+    console.log('Checking Cassandra status again..');
+  })
+
+
+
+  // checkcmd.kill('SIGINT');
