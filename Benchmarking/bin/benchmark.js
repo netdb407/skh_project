28ecdb2a3033b470330c6fe8b5cd910124fac741
smlee-git <ppsoming@gamil.com>
2020-07-14 02:47:11 +0900

    nosqltestes name, time 옵션

--- Benchmarking/bin/benchmark.js
+++ Benchmarking/bin/benchmark.js
@@ -39,7 +39,7 @@ program
   // .option('-o, --output <output>', `assign output directory (server, node)`)
   .option('-s, --timewindow <timewindow>', `insert time window (sec) (default : 1 sec)`)
   .option('-t, --threads <threads>',`insert number of threads (default : 1)`)
-  .option('-m, --remove', `remove arangodb data before running benchmark (default : off)`)
+  // .option('-m, --remove', `remove arangodb data before running benchmark (default : off)`)
   // .option('-c, --casstracing',`enable the cassandra tracing option (default : off)`)
   .option('-i, --iotracer',`run iotracing option (default : off)`)
   .option('-e, --event <event>', `Add events by specifying a percentage. (default : 0)`)
@@ -51,47 +51,46 @@ program
 
 program
   .command('orientdb')
-  // .option('-d, --dbtype <dbtype>', `insert dbtype, choose from 'cassandra', 'arangodb', 'orientdb'`)
-  .option('-r, --runtype <runtype>', `insert runtype, choose from 'load', 'run', 'loadrun'`)
-  .option('-l, --loadsize <loadsize>', `insert load size [###M, ###G, ###T]`)
-  .option('-w, --wlfile <wlfile>', `insert workload file type or name [type:news, contents, facebook, log, recommendation ..]`)
-  // .option('-c --config <config>', `config 파일 입력`)
-  .option('-n, --name <name>', `insert benchmark name`)
-  // .option('-o, --output <output>', `assign output directory (server, node)`)
-  // .option('-s, --timewindow <timewindow>', `insert time window (sec) (default : 1 sec)`)
-  // .option('-t, --threads <threads>',`insert number of threads (default : 1)`)
-  // .option('-m, --remove', `remove cassandra data before running benchmark (default : off)`)
-  // .option('-c, --casstracing',`enable the cassandra tracing option (default : off)`)
-  .option('-i, --iotracer',`run iotracing option (default : off)`)
+  .option('-n, --name <name>', `insert workload file name`)
+  .option('-t, --time <time>', `insert set time`)
+  .option('-s, --size <size>', `insert load size`)
+  .option('-r, --remove <remove>', `remove data`)
   .action(function(opt){
     exec('chmod -R +x .')
-    opt.dbtype = 'orientdb'
-    nosqlTests.graphbench(opt);
+
+    if((typeof(opt.name)== 'string') && (typeof(opt.time)) =='string'){ // 필수 옵션이 있으면 실행 ,, 
+      nosqlTests.graphbench(opt);
+    }else{
+      console.log('----------------------------------------------------------');
+      console.log(chalk.red.bold('[ERROR]'), 'insert necessary option :', chalk.blue.bold('-n'),',', chalk.blue.bold('-t'));
+      console.log('----------------------------------------------------------');
+    }
+
 })
 
 
 program.parse(process.argv);
 // program2.parse(process.argv);
 
-function checkDBtype(opt){
-  exec('chmod -R +x .')
-  // workloadProperty(opt)
-
-  let dbtypeInfo = chalk.magenta('dbtype')
-  switch(opt.dbtype){
-    case 'cassandra' :
-      ycsbAction.ycsb(opt);
-      break;
-    case 'arangodb' :
-      ycsbAction.ycsb(opt);
-      break;
-    case 'orientdb' :
-      // graphbenchAction.graphbench(opt)
-      nosqlTests.graphbench(opt);
-      break;
-    default :
-      let dbtypeLine = `${error} ${dbtypeInfo} : invalid choice ${opt.dbtype}, (choose from 'cassandra', 'arangodb', 'orientdb')`
-      console.log(dbtypeLine)
-      break;
-    }
-}
+// function checkDBtype(opt){
+//   exec('chmod -R +x .')
+//   // workloadProperty(opt)
+//
+//   let dbtypeInfo = chalk.magenta('dbtype')
+//   switch(opt.dbtype){
+//     case 'cassandra' :
+//       ycsbAction.ycsb(opt);
+//       break;
+//     case 'arangodb' :
+//       ycsbAction.ycsb(opt);
+//       break;
+//     case 'orientdb' :
+//       // graphbenchAction.graphbench(opt)
+//       nosqlTests.graphbench(opt);
+//       break;
+//     default :
+//       let dbtypeLine = `${error} ${dbtypeInfo} : invalid choice ${opt.dbtype}, (choose from 'cassandra', 'arangodb', 'orientdb')`
+//       console.log(dbtypeLine)
+//       break;
+//     }
+// }
--- Benchmarking/lib/nosqlTests.js
+++ Benchmarking/lib/nosqlTests.js
@@ -1,4 +1,105 @@
+const PropertiesReader = require('properties-reader');
+const property = require('../../propertiesReader.js')
+const chalk = require('chalk')
+const exec = require('child_process').exec
+const fs = require('fs')
+const workload_dir = property.get_server_file2_dir() // Graph/workloads
+const result_dir = property.get_server_file3_dir() // Graph/result
+const ssdStorage_dir = property.get_IO_watch_dir()
+let status = -1 //켜져있을 때 1, 꺼져있을 때 -1, stderr일때 0
+const orientdir = property.server_orientdb_dir();
+let orientdbdir = orientdir.split(',')
+const hosts = property.get_nodes_IP()
+let ip = hosts.split(',')
+
 module.exports.graphbench = (opt) => {
 
-  console.log('nosqlTests');
+  runNosquTests(status)
+  // console.log(opt);
+  async function runNosquTests(status) {
+    let name_status = await checkStatus_name(status, opt.name)
+    // console.log('NAME', name)
+    let time_status = await checkStatus_time(status, opt.time)
+    // console.log('TIME', time)
+
+    // name, time 등 등 옵션의 값들이 1일 때 =>
+    if (name_status == 1 && time_status == 1) { //
+      try {
+        // console.log(`nosqltest1 orientdb -n ${opt.name} -t ${opt.time}`);
+        console.log('----------------------------------------------------------');
+        console.log(chalk.green.bold('[INFO]'), 'Start orientdb benchmarking');
+        console.log('----------------------------------------------------------');
+        let runexec = exec(`nosqltest1 orientdb -n ${opt.name} -t ${opt.time}`)
+
+        runexec.stdout.on('data', function(data){
+          console.log(data);
+        })
+        runexec.on('exit', function(data){
+          try{
+            ip.forEach((i) => {
+              dirnum = i.split('.');
+              const std = exec(`ssh root@${i} ${ssdStorage_dir}/orientdb${dirnum[dirnum.length-1]}/killOrient.sh`);
+              console.log('--------------------------------------');
+              console.log(chalk.green.bold('[INFO]'), 'orientdb run : ', chalk.blue.bold(i));
+              console.log('--------------------------------------');
+            });
+
+          }catch (err) {
+            console.error(err);
+          }
+
+        })
+
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log('----------------------------------------------------------');
+      console.log(chalk.red.bold('[ERROR]'), 'There was an error and could not be executed.')
+      console.log('----------------------------------------------------------');
+    }
+  }
+}
+
+function checkStatus_name(status, name) {
+  return new Promise(function(resolve, reject) {
+    let file = `./${workload_dir}${name}`
+    // console.log('FILE', file)
+    try {
+      fs.statSync(file);
+      console.log('----------------------------------------------------------');
+      console.log(chalk.green.bold('[INFO]'), 'workload name :', chalk.blue.bold(name));
+      console.log('----------------------------------------------------------');
+      return resolve(status * -1) //success : 1
+    } catch (err) {
+      // console.error(err);
+      console.log('----------------------------------------------------------');
+      console.log(chalk.red.bold('[ERROR]'), 'workload name :', 'invalid workload name', `'${name}'`)
+      console.log('----------------------------------------------------------');
+      return resolve(status) //fail : -1
+    }
+  });
+}
+
+function isNumber(s) { // 입력이 숫자인지 확인해주는 함수
+  s += ''; // 문자열로 변환
+  s = s.replace(/^\s*|\s*$/g, ''); // 좌우 공백 제거
+  if (s == '' || isNaN(s)) return false;
+  return true;
+}
+
+function checkStatus_time(status, time) {
+  return new Promise(function(resolve, reject) {
+      if (isNumber(time)) { // 입력이 숫자
+        console.log('----------------------------------------------------------');
+        console.log(chalk.green.bold('[INFO]'), 'settime :', chalk.blue.bold(time));
+        console.log('----------------------------------------------------------');
+        return resolve(status * -1) //success : 1
+      } else { // 입력이 숫자가 아님
+        console.log('----------------------------------------------------------');
+        console.log(chalk.red.bold('[ERROR]'), 'settime :', `insert 'number' type.`)
+        console.log('----------------------------------------------------------');
+        return resolve(status) //fail : -1
+      }
+  });
 }
