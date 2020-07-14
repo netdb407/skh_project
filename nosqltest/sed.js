c0a3c83310836914f0d840a9fd90c2d14870df41
rud5356 <59816321+rud5356@users.noreply.github.com>
2020-07-14 18:06:34 +0900

    bb
    bb

--- nosqltest/sed.js
+++ nosqltest/sed.js
@@ -4,11 +4,15 @@ const execSync = require('child_process').execSync;
 const exec = require('child_process').exec;
 const property = require('../propertiesReader.js')
 const PropertiesReader = require('properties-reader');
-
+const orientMaster_IP = property.get_orientMaster_IP();
+const profile_dir = property.get_profile_dir();
+const relation_dir = property.get_relation_dir();
 async function sss(){
-  const std = exec(`ssh root@193.255.92.195 ${IO_watch_dir}/orientdb${dirnum[dirnum.length-1]}/bin/dserver.sh &`);
-  console.log('--------------------------------------');
-  console.log(chalk.green.bold('[INFO]'), 'orientdb run : ', chalk.blue.bold(i));
-  console.log('--------------------------------------');
+  const std1 = exec(`ssh root@${orientMaster_IP} 'sed -i "s|"/root/ssdStorage/orientdb195/person/person_knows.csv"|"${profile_dir}"|"' /root/ssdStorage/orientdb195/bin/person.json`);
+  const std2 = exec(`ssh root@${orientMaster_IP} 'sed -i "s|"/root/ssdStorage/orientdb195/person/person_knows.csv"|"${relation_dir}"|"' /root/ssdStorage/orientdb195/bin/person_knows.json`);
+  std1;
+  std2;
+  console.log('변경완료');
 }
 sss();
+process.exit(1);
